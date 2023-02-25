b9ebfe6c93704d7ff7d912f4b78d96d6d2969484
Yixuan Feng <56617127+yi-xuan-97@users.noreply.github.com>
2023-02-25 10:55:52 -0800

    update on navbar

--- src/Page/NavBar.js
+++ src/Page/NavBar.js
@@ -7,52 +7,71 @@ import {
   makeStyles,
 } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 const useStyles = makeStyles((theme) => ({
   navlinks: {
-    marginTop: theme.spacing(1),
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  icons: {
+    marginLeft: theme.spacing(35),
+  },
+  icon: {
+    color: "white",
+    height: theme.spacing(5),
+    width: theme.spacing(5),
+    marginLeft: theme.spacing(4),
   },
   link: {
     textDecoration: "none",
     color: "white",
-    fontSize: "20px",
-    marginRight: theme.spacing(10),
+    fontSize: "22px",
+    marginRight: theme.spacing(8),
     "&:hover": {
       color: "yellow",
+      fontWeight: "700",
       borderBottom: "1px solid white",
     },
   },
 }));
 
 function NavBar() {
-
   const classes = useStyles();
 
-  return(
+  return (
     <AppBar position="static" style={{ backgroundColor: "#0066cc" }}>
-    <CssBaseline />
-    <Toolbar>
-      <div className={classes.navlinks}>
-        <Link to="/" className={classes.link}>
-        </Link>
-        <Link to="/Home" className={classes.link}>
-          Home
-        </Link>
-        <Link to="/About" className={classes.link}>
-          About
-        </Link>
-        <Link to="/PreviousWork" className={classes.link}>
-          Previous Work
-        </Link>
-        <Link to="/Project" className={classes.link}>
-          Project
-        </Link>
-        <Link to="/Contact" className={classes.link}>
-          Contact
-        </Link>
-      </div>
-    </Toolbar>
-  </AppBar>
+      <CssBaseline />
+      <Toolbar>
+        <div className={classes.navlinks}>
+          <Link to="/" className={classes.link}></Link>
+          <Link to="/Home" className={classes.link}>
+            Home
+          </Link>
+          <Link to="/About" className={classes.link}>
+            About
+          </Link>
+          <Link to="/Project" className={classes.link}>
+            Projects
+          </Link>
+          <Link to="/PreviousWork" className={classes.link}>
+            Resume
+          </Link>
+          <Link to="/Contact" className={classes.link}>
+            Contact Me
+          </Link>
+          <div className={classes.icons}>
+            <FontAwesomeIcon className={classes.icon} icon={faLinkedin} />
+            <FontAwesomeIcon className={classes.icon} icon={faGithub} />
+            <FontAwesomeIcon className={classes.icon} icon={faEnvelope} />
+          </div>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 }
 
